3029d7c5c9c8fceb7d28611d5ef008fc77193183
Costin <ghenea.costin@gmail.com>
2025-11-11 19:43:04 +0200

    Adaugare constanta

--- Backend/src/main/java/com/example/Backend/JWT/JwtUtil.java
+++ Backend/src/main/java/com/example/Backend/JWT/JwtUtil.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
+    public static final Date TIMP_de_peste_o_ora = new Date(System.currentTimeMillis() + 1000L * 60 * 60);
     private static final String SECRET_KEY =
             "ThisIsASecretKeyForJWTWithAtLeast256BitsLength!";
     public String generateToken(String userId, String role) {
@@ -20,7 +21,7 @@ public class JwtUtil {
                 .claim("role"
                         , role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(TIMP_de_peste_o_ora)
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
