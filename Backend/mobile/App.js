202c36fb24416d2338b5d312f9d2085fca7a3e35
Costin <ghenea.costin@gmail.com>
2025-12-16 19:36:57 +0200

    Update/Delete Treatments

--- Backend/mobile/App.js
+++ Backend/mobile/App.js
@@ -1,47 +1,25 @@
-import React, { useState } from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [taken, setTaken] = useState(false);
-
   return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Aplicație pacient – test simplu</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
 
-        <View style={styles.switchRow}>
-          <Text style={styles.label}>
-            Doză administrată: {taken ? 'Da' : 'Nu '}
-          </Text>
-          <Switch
-              value={taken}
-              onValueChange={setTaken}
+          <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
           />
-        </View>
-      </View>
+
+
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#ffffff',
-  },
-  title: {
-    fontSize: 22,
-    marginBottom: 40,
-    fontWeight: 'bold',
-    color: '#000000',
-  },
-  switchRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  label: {
-    fontSize: 18,
-    marginRight: 10,
-    color: '#000000',
-  },
-});
