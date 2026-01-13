e7d57f9ea437e47824348ae34a8346fc30268ac9
Costin <ghenea.costin@gmail.com>
2026-01-13 20:38:34 +0200

    Added NOtifications

--- Backend/mobile/App.js
+++ Backend/mobile/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform, Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -15,7 +17,48 @@ enableScreens();
 
 const Stack = createNativeStackNavigator();
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
+
+
+
 export default function App() {
+
+
+    useEffect(() => {
+        (async () => {
+
+            if (Platform.OS === 'android') {
+                await Notifications.setNotificationChannelAsync('default', {
+                    name: 'default',
+                    importance: Notifications.AndroidImportance.MAX,
+                    vibrationPattern: [0, 250, 250, 250],
+                    lightColor: '#FF231F7C',
+                });
+            }
+
+
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+
+
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+
+            if (finalStatus !== 'granted') {
+                Alert.alert('Atenție', 'Nu vom putea trimite notificări pentru tratamente fără permisiune!');
+                return;
+            }
+        })();
+    }, []);
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Login">
